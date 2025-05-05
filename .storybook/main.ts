145a0faf5f7203fdb08215487dd35d7f06d20015
takemura <170946913+shikano35@users.noreply.github.com>
2025-05-05 12:23:43 +0900

    chore: setup Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-coverage',
+    '@storybook/addon-mdx-gfm',
+  ],
+
+  framework: {
+    name: '@storybook/experimental-nextjs-vite',
+    options: {},
+  },
+
+  docs: {
+    autodocs: 'tag',
+  },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
+};
+export default config;
