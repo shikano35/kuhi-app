3f9449347e109570945b2af5641d68306fb554fc
takemura <170946913+shikano35@users.noreply.github.com>
2025-05-18 23:36:04 +0900

    feat: add base metadata

---
+++ src/lib/metadata.ts
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next';
+
+export const baseMetadata: Metadata = {
+  title: 'kuhi-app',
+  description: '句碑の情報を使用したWebアプリケーションです。',
+  keywords: 'kuhi-app,kuhi,haiku-monument,句碑,句碑アプリ',
+  generator: '',
+  applicationName: '',
+  openGraph: {
+    title: '',
+    description: '',
+    type: 'website',
+    url: '',
+    siteName: '',
+    locale: 'ja_JP',
+    images: [
+      {
+        url: '',
+        width: 1200,
+        height: 630,
+        alt: '',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary',
+    title: '',
+    description: '',
+  },
+};
