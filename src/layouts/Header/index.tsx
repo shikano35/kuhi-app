1567cc0820eb3ec3993c0cd7a28b579824bc7c7a
takemura <170946913+shikano35@users.noreply.github.com>
2025-05-20 00:28:27 +0900

    fix: update Header component

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,7 +1,98 @@
+import { Button } from '@/components/ui/button';
+import { MenuIcon } from 'lucide-react';
+import Link from 'next/link';
+
+const headerLinks = [
+  {
+    label: 'ホーム',
+    href: '/',
+  },
+  {
+    label: '句碑マップ',
+    href: '/map',
+  },
+  {
+    label: '句碑リスト',
+    href: '/list',
+  },
+  {
+    label: '句碑とは',
+    href: '/about',
+  },
+];
+
+export function HomeHeader() {
+  return (
+    <header className="absolute top-0 left-0 right-0 z-50">
+      <div className="container mx-auto flex items-center justify-between p-4 py-8">
+        <div className="flex items-center">
+          <Link className="flex items-center" href="/">
+            <span className="text-white text-2xl font-bold">くひめぐり</span>
+          </Link>
+
+          <nav className="hidden md:flex space-x-8 ml-12 lg:ml-24">
+            {headerLinks.map((link, index) => (
+              <Link
+                className="text-white hover:text-gray-300 transition-colors font-medium"
+                href={link.href}
+                key={index}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </nav>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <Button
+            asChild
+            className="text-md bg-white/0 text-white hover:bg-white/5 hover:text-white rounded-full"
+            variant="outline"
+          >
+            <Link href="/login">ログイン</Link>
+          </Button>
+          <button className="md:hidden">
+            <MenuIcon className="w-6 h-6 text-white" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
 export function Header() {
   return (
-    <header>
-      <div>Header</div>
+    <header className="top-0 left-0 right-0 z-50 shadow-sm">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        <div className="flex items-center">
+          <Link className="flex items-center" href="/">
+            <span className="text-2xl font-bold">くひめぐり</span>
+          </Link>
+
+          <nav className="hidden md:flex space-x-8 ml-12 lg:ml-24">
+            {headerLinks.map((link, index) => (
+              <Link
+                className="text-primary hover:text-muted-foreground transition-colors font-medium"
+                href={link.href}
+                key={index}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </nav>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          {/* <Button asChild className="text-md rounded-full">
+            <Link href="/login">
+              ログイン
+            </Link>
+          </Button> */}
+          <button className="md:hidden">
+            <MenuIcon className="w-6 h-6" />
+          </button>
+        </div>
+      </div>
     </header>
   );
 }
