7866f84990f1d0d5be7876494e2fb882c468a68c
takemura <170946913+shikano35@users.noreply.github.com>
2025-05-25 15:36:54 +0900

    fix: simplify login page and refactor LoginForm component

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,4 @@
 import { LoginForm } from '@/components/Auth/LoginForm';
-import { Info } from 'lucide-react';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -10,30 +9,8 @@ export const metadata: Metadata = {
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-muted/50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg-muted/50 px-4">
       <div className="max-w-md w-full space-y-8">
-        <div className="text-center">
-          <h1 className="mt-6 text-3xl font-extrabold text-primary">
-            ログイン
-          </h1>
-          <p className="mt-2 text-sm text-muted-foreground">
-            句碑情報の投稿やお気に入り登録ができます。
-          </p>
-        </div>
-        <div className="bg-yellow-50 border-l-2 border-yellow-400 p-2 my-6 max-w-xs mx-auto">
-          <div className="flex justify-center">
-            <div className="flex-shrink-0">
-              <Info className="size-5 text-yellow-400" />
-            </div>
-            <div className="ml-4">
-              <p className="text-sm text-yellow-700 text-start">
-                現在，句碑情報の投稿のみ可能です。
-                <br />
-                お気に入り登録は利用できません。
-              </p>
-            </div>
-          </div>
-        </div>
         <LoginForm />
       </div>
     </div>
