c97f260d9372dbc4b7f3383dacb5bb63784e0842
takemura <170946913+shikano35@users.noreply.github.com>
2025-05-30 21:39:05 +0900

    fix: update LoginForm

--- src/components/Auth/LoginForm/index.tsx
+++ src/components/Auth/LoginForm/index.tsx
@@ -3,12 +3,13 @@
 import { useState } from 'react';
 import { signIn } from 'next-auth/react';
 import { Info } from 'lucide-react';
-import { GoogleIcon } from '@/components/Icon/GoogleIcon';
 
 export function LoginForm() {
+  // eslint-disable-next-line unused-imports/no-unused-vars
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
+  // eslint-disable-next-line unused-imports/no-unused-vars
   const handleGoogleLogin = async () => {
     setIsLoading(true);
     try {
@@ -31,32 +32,11 @@ export function LoginForm() {
           句碑情報の投稿やお気に入り登録ができます。
         </p>
       </div>
-      <div className="bg-yellow-50 border-l-2 border-yellow-400 p-2 my-6 max-w-sm mx-auto">
-        <div className="flex justify-center">
-          <div className="flex-shrink-0">
-            <Info className="size-5 text-yellow-400" />
-          </div>
-          <div className="ml-4">
-            <p className="text-sm md:text-base text-yellow-700 text-start">
-              現在，句碑情報の投稿のみ可能です。
-              <br />
-              お気に入り登録は利用できません。
-            </p>
-          </div>
-        </div>
-      </div>
       {error && (
         <div className="mb-4 bg-red-50 border-l-4 border-red-400 p-4">
           <div className="flex">
             <div className="flex-shrink-0">
-              <svg
-                className="h-5 w-5 text-red-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <Info className="size-5 text-destructive" />
-              </svg>
+              <Info className="size-5 mt-0.75 text-destructive" />
             </div>
             <div className="ml-3">
               <p className="text-sm text-red-700">{error}</p>
@@ -65,14 +45,15 @@ export function LoginForm() {
         </div>
       )}
       <div className="mt-4 flex justify-center">
-        <button
+        {/* <button
           className="w-3/4 text-base md:text-lg flex justify-center items-center py-2 px-4 border border-border rounded-full shadow-sm font-semibold text-primary bg-background hover:bg-muted hover:cursor-pointer"
           disabled={isLoading}
           onClick={handleGoogleLogin}
         >
-          <GoogleIcon className="size-5 mr-4 mt-1" />
+          <GoogleIcon className="size-5 mr-4 mt-0.75" />
           Googleでログイン
-        </button>
+        </button> */}
+        <p>現在，プライバシーポリシーを準備中です</p>
       </div>
       <div className="mt-6 text-center" />
     </div>
